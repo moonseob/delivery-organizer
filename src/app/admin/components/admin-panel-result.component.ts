90d26098cb57daef962404fc870099d045d5e9d3
Moonsub Um <moonsub.um@probit.com>
2022-03-24 12:33:18 +0900

    admin: 장바구니 수정 코드 생성 최적화

--- src/app/admin/components/admin-panel-result.component.ts
+++ src/app/admin/components/admin-panel-result.component.ts
@@ -1,6 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CartItem, CheckoutAddress } from 'src/app/shared/models/cart.model';
 import { Restaurant } from 'src/app/shop/models/shop-info.model';
 import { ShopApiService } from 'src/app/shop/services/shop-api.service';
@@ -10,63 +8,67 @@ import { ShopApiService } from 'src/app/shop/services/shop-api.service';
   templateUrl: './admin-panel-result.component.html',
   styleUrls: ['./admin-panel-result.component.scss'],
 })
-export class AdminPanelResultComponent implements OnChanges, OnInit {
+export class AdminPanelResultComponent implements OnChanges {
   constructor(private service: ShopApiService) {}
 
   @Input() shopId!: string;
   @Input() address!: CheckoutAddress;
   @Input() data!: CartItem[];
 
-  private shopInfo$!: Observable<Restaurant>;
+  private shopInfo?: Restaurant;
 
+  ready = false;
   resultCode = '';
 
-  ngOnInit(): void {
-    this.shopInfo$ = this.service.getInfo(this.shopId).pipe(shareReplay(1));
-  }
-
-  async ngOnChanges() {
-    const shopInfo = await this.shopInfo$?.toPromise();
+  async ngOnChanges(change: SimpleChanges) {
     const ssKey = 'ngStorage-cart';
-    // this.getCodeString(ssKey)
-    this.resultCode = `
-    const cartItemReducer = (acc, cur) => {
-      console.log('acc: ', acc, 'cur: ', cur)
-      const match = acc.findIndex(
-        (item) =>
-          item.slug === cur.slug &&
-          JSON.stringify(item.subchoices) === JSON.stringify(cur.subchoices),
-      );
-      console.log('match: ', match);
-      if (match >= 0) {
-        const prev = acc[match];
-        acc[match] = { ...prev, amount: prev.amount + 1 };
-      } else {
-        acc = acc.concat({ ...cur, id_for_stock: new Date().valueOf() });
-      }
-      console.log('acc result: ', acc);
-      return acc;
-    };
-    (function() {
-      const existingCart = JSON.parse(sessionStorage.getItem('${ssKey}') || '{}');
-      const restaurant_id = Number(${this.shopId});
-      const isSameOrigin = existingCart.restaurant_id === restaurant_id;
-      const restaurant = ${JSON.stringify(shopInfo)};
-      const restaurant_name = restaurant.name;
-      const items = (isSameOrigin ? existingCart.items : []).concat(${JSON.stringify(
-        this.data
-      )}).reduce(cartItemReducer, []);
-      const result = {
-        restaurant_id,
-        restaurant,
-        restaurant_name,
-        checkout_address: ${JSON.stringify(this.address)},
-        items,
-        default_price: 0,
-      };
-      sessionStorage.setItem('${ssKey}', JSON.stringify(result));
-      window.location.reload();
-    })()`;
+    if (change.shopId) {
+      this.shopInfo = await this.service.getInfo(this.shopId).toPromise();
+    }
+    console.log(this.shopInfo, this.address, this.data);
+    this.ready =
+      (this.shopInfo && this.address && this.data?.length > 0) || false;
+    if (this.ready) {
+      console.log('update resultCode');
+      this.resultCode = `
+        const cartItemReducer = (acc, cur) => {
+          console.log('acc: ', acc, 'cur: ', cur)
+          const match = acc.findIndex(
+            (item) =>
+              item.slug === cur.slug &&
+              JSON.stringify(item.subchoices) === JSON.stringify(cur.subchoices),
+          );
+          console.log('match: ', match);
+          if (match >= 0) {
+            const prev = acc[match];
+            acc[match] = { ...prev, amount: prev.amount + 1 };
+          } else {
+            acc = acc.concat({ ...cur, id_for_stock: new Date().valueOf() });
+          }
+          console.log('acc result: ', acc);
+          return acc;
+        };
+        (function() {
+          const existingCart = JSON.parse(sessionStorage.getItem('${ssKey}') || '{}');
+          const restaurant_id = Number(${this.shopId});
+          const isSameOrigin = existingCart.restaurant_id === restaurant_id;
+          const restaurant = ${JSON.stringify(this.shopInfo)};
+          const restaurant_name = restaurant.name;
+          const items = (isSameOrigin ? existingCart.items : []).concat(${JSON.stringify(
+            this.data
+          )}).reduce(cartItemReducer, []);
+          const result = {
+            restaurant_id,
+            restaurant,
+            restaurant_name,
+            checkout_address: ${JSON.stringify(this.address)},
+            items,
+            default_price: 0,
+          };
+          sessionStorage.setItem('${ssKey}', JSON.stringify(result));
+          window.location.reload();
+        })()`;
+    }
   }
 
   copied(e: any) {
